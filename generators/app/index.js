57fb815bc3875ae5e193fc4130bb85f4f6b3379a
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2017-06-22 02:37:57 -0300

    Generate new mongodb default password based on random uuid

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,6 +3,8 @@ const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
 const path = require('path');
+const uuid = require('uuid/v4');
+var replace = require('lodash.replace');
 
 module.exports = class extends Generator {
   prompting() {
@@ -11,23 +13,27 @@ module.exports = class extends Generator {
       'Welcome to ' + chalk.red('Node API 1st class experience with Docker and VS Code') + ' generator!'
     ));
 
+
+
     const prompts = [{
       type: 'input',
       name: 'projectName',
       message: 'Your project name',
       default: this.appname // Default to current folder name
-    },{
+    }, {
       type: 'input',
       name: 'projectDescription',
       message: 'Project Description?'
-    },{
+    }, {
       type: 'input',
       name: 'mongodbUsername',
-      message: 'MongoDB root username'
-    },{
+      message: 'MongoDB root username',
+      default: "app_mongo_user"
+    }, {
       type: 'input',
       name: 'mongodbPassword',
-      message: 'MongoDB root password'
+      message: 'MongoDB root password',
+      default: this.passwordGen()
     }];
 
     return this.prompt(prompts).then(props => {
@@ -47,9 +53,14 @@ module.exports = class extends Generator {
       this.destinationPath('.vscode/'),
       this.props
     );
-    this.fs.move(this.destinationPath('Dockerfile.all.projectName-api-base'),this.destinationPath(`Dockerfile.all.${this.props.projectName}-api-base`));
-    this.fs.move(this.destinationPath('Dockerfile.debug.projectName-api'),this.destinationPath(`Dockerfile.debug.${this.props.projectName}-api`));
-    this.fs.move(this.destinationPath('Dockerfile.release.projectName-api'),this.destinationPath(`Dockerfile.release.${this.props.projectName}-api`));
+    this.fs.move(this.destinationPath('Dockerfile.all.projectName-api-base'), this.destinationPath(`Dockerfile.all.${this.props.projectName}-api-base`));
+    this.fs.move(this.destinationPath('Dockerfile.debug.projectName-api'), this.destinationPath(`Dockerfile.debug.${this.props.projectName}-api`));
+    this.fs.move(this.destinationPath('Dockerfile.release.projectName-api'), this.destinationPath(`Dockerfile.release.${this.props.projectName}-api`));
+  }
+
+  passwordGen() {
+    const newPassword = replace(replace(replace(replace(uuid(), "-", ""), "-", ""), "-", ""), "-", "");
+    return newPassword;
   }
 
   install() {
